f0685e74d5a14c40759e7a4b7db0cdd4966ee002
WEWEDIRKA1 <gosalasin161@gmail.com>
2025-08-26 18:45:48 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,10 +7,11 @@ wait(0.5)
 
 -- Attempt to kick the player
 local success, errorMessage = pcall(function()
-    LocalPlayer:Kick("Join our new Discord server: https://discord.gg/tcKh9V79xf By GendioHub")
+    LocalPlayer:Kick("Join our new Discord server: https://discord.gg/t45UCWPGcm By GendioHub")
 end)
 
 -- Debug output if the kick fails
 if not success then
     warn("Failed to kick player: " .. tostring(errorMessage))
 end
+
